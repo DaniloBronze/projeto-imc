6e09ab6e1792fa72c3170b5fbf8c6ff151ad2ca8
Danilo Bronze <74279443+DaniloBronze@users.noreply.github.com>
2024-07-25 16:29:50 -0300

    update

--- public/script.js
+++ public/script.js
@@ -1,41 +1,38 @@
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
-function initCalculaImc() {
-    const forms = document.querySelectorAll('.needs-validation');
+function getFormValues() {
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    const termoAceito = document.getElementById('invalidCheck').checked;
+    const genero = document.getElementById('genero').value;
+    const listaAfazeres = document.getElementById('lista-afazeres').value;
 
-    async function calculaImc(event) {
-        event.preventDefault();
+    return { peso, altura, termoAceito, genero, listaAfazeres };
+}
 
-        // Pegue os valores dos campos
-        const peso = parseFloat(document.getElementById('peso').value);
-        const altura = parseFloat(document.getElementById('altura').value);
-        const termoAceito = document.getElementById('invalidCheck').checked;
-        const genero = document.getElementById('genero').value;
-        const listaAfazeres = document.getElementById('lista-afazeres').value;
+function isValidForm({ peso, altura, termoAceito, genero, listaAfazeres }) {
+    return !isNaN(peso) && !isNaN(altura) && termoAceito && genero && listaAfazeres;
+}
 
-        // Verifique se todos os campos estão preenchidos e se o termo foi aceito
-        if (isNaN(peso) || isNaN(altura) || !termoAceito || !genero || !listaAfazeres) {
-            alert('Por favor, insira valores válidos para peso e altura, aceite os termos de uso e preencha todos os campos.');
-            return;
-        }
-
-        // Calcule o IMC
-        const imc = peso / (altura * altura);
-        const resultado = document.querySelector('code');
-        exibeImc(imc, resultado, genero, listaAfazeres);
-    }
-
-    forms.forEach((form) => {
-        form.addEventListener('submit', calculaImc);
-    });
+function calculateImc(peso, altura) {
+    return peso / (altura * altura);
 }
 
 function exibeImc(imc, resultadoElement, genero, listaAfazeres) {
     if (resultadoElement) {
-        let mensagem;
-        let consulta;
-        let mensagensAdicionais = [];
+        const { mensagem, consulta, mensagensAdicionais } = getImcMessage(imc, genero, listaAfazeres);
 
+        resultadoElement.textContent = `Seu IMC é: ${imc.toFixed(2)} (${mensagem})`;
+
+        if (consulta) {
+            runConsulta(consulta, mensagensAdicionais);
+        }
+    }
+}
+
+function getImcMessage(imc, genero, listaAfazeres) {
+    let mensagem, consulta = null, mensagensAdicionais = null
+    
         if (imc < 18.5) {
             mensagem = "Abaixo do peso";
             consulta = `como ganhar massa muscular para ${genero}`;
@@ -53,8 +50,7 @@ function exibeImc(imc, resultadoElement, genero, listaAfazeres) {
             ];
         } else if (imc >= 18.5 && imc < 24.9) {
             mensagem = "Peso normal";
-            resultadoElement.textContent = `Seu IMC é: ${imc.toFixed(2)} (${mensagem})`;
-            return;
+            return { mensagem: `(${mensagem})` };
         } else if (imc >= 25 && imc < 29.9) {
             mensagem = "Sobrepeso";
             consulta = `como emagrecer para ${genero}`;
@@ -94,31 +90,45 @@ function exibeImc(imc, resultadoElement, genero, listaAfazeres) {
         if (listaAfazeres === 'Sim') {
             consulta += ` e mande uma lista de afazeres para gênero ${genero}`;
         }
-
-        resultadoElement.textContent = `Seu IMC é: ${imc.toFixed(2)} (${mensagem})`;
-        run(consulta, mensagensAdicionais);
-    }
+        
+        return { mensagem, consulta, mensagensAdicionais };
+    
 }
 
-async function run(consulta, mensagensAdicionais) {
+async function runConsulta(consulta, mensagensAdicionais) {
     try {
         const response = await fetch(`/api/generate?prompt=${encodeURIComponent(consulta)}`);
         const result = await response.json();
         const htmlResponse = marked.parse(result.response);
 
-        // Escolha uma mensagem adicional aleatória
         const mensagemAleatoria = mensagensAdicionais[Math.floor(Math.random() * mensagensAdicionais.length)];
-        const nota = document.querySelector('.piada');
-        nota.innerText = mensagemAleatoria;
+        document.querySelector('.piada').innerText = mensagemAleatoria;
 
         const resultado = document.querySelector('code');
         resultado.innerHTML += `<br><strong>Dica:</strong> ${htmlResponse}`;
     } catch (error) {
         console.error('Erro ao obter resposta do Gemini:', error);
-        const resultado = document.querySelector('code');
-        resultado.innerHTML += `<br>Desculpe, houve um problema ao obter a recomendação.`;
+        document.querySelector('code').innerHTML += `<br>Desculpe, houve um problema ao obter a recomendação.`;
     }
 }
+
+
+function initCalculaImc() {
+    const forms = document.querySelectorAll('.needs-validation');
+    forms.forEach((form) => {
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            const { peso, altura, termoAceito, genero, listaAfazeres } = getFormValues();
+            if (isValidForm({ peso, altura, termoAceito, genero, listaAfazeres })) {
+                const imc = calculateImc(peso, altura);
+                exibeImc(imc, document.querySelector('code'), genero, listaAfazeres);
+            } else {
+                alert('Por favor, insira valores válidos para peso e altura, aceite os termos de uso e preencha todos os campos.');
+            }
+        });
+    });
+}
+
 
 
 function initThemeDark() {
@@ -126,9 +136,10 @@ function initThemeDark() {
     <svg id="moon-icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-moon" viewBox="0 0 16 16">
         <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286"/>
     </svg>`;
-    const newMoonIcon = `<svg id="moon-icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16">
-  <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"/>
-</svg>`;
+    const newMoonIcon = `
+    <svg id="moon-icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16">
+        <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"/>
+    </svg>`;
 
     function toggleTheme() {
         const theme = document.body;
@@ -137,7 +148,6 @@ function initThemeDark() {
 
         moonIcon.outerHTML = isDarkMode ? newMoonIcon : originalMoonIcon;
 
-        // Reattach event listener to the new moon icon
         document.getElementById('moon-icon').addEventListener('click', toggleTheme);
 
         localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
@@ -152,8 +162,6 @@ function initThemeDark() {
     }
 
     loadTheme();
-
-    // Adicionar o evento ao ícone de alternância do tema
     const moonIcon = document.getElementById('moon-icon');
     if (moonIcon) {
         moonIcon.addEventListener('click', toggleTheme);
